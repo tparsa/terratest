2e5e09331d264593f3db036a718f23cc26cd3da1
Parsa <parsa_ghorbani@outlook.com>
2023-05-06 19:52:01 +0330

    feat(modules/helm): Render remote templates

--- modules/helm/template.go
+++ modules/helm/template.go
@@ -25,13 +25,16 @@ func RenderTemplate(t testing.TestingT, options *Options, chartDir string, relea
 // RenderTemplateE runs `helm template` to render the template given the provided options and returns stdout/stderr from
 // the template command. If you pass in templateFiles, this will only render those templates.
 func RenderTemplateE(t testing.TestingT, options *Options, chartDir string, releaseName string, templateFiles []string, extraHelmArgs ...string) (string, error) {
-	// First, verify the charts dir exists
-	absChartDir, err := filepath.Abs(chartDir)
-	if err != nil {
-		return "", errors.WithStackTrace(err)
-	}
-	if !files.FileExists(chartDir) {
-		return "", errors.WithStackTrace(ChartNotFoundError{chartDir})
+	// If the chart refers to a path, convert to absolute path. Otherwise, pass straight through as it may be a remote
+	// chart.
+	isLocalChart := false
+	if files.FileExists(chartDir) {
+		absChartDir, err := filepath.Abs(chartDir)
+		if err != nil {
+			return "", errors.WithStackTrace(err)
+		}
+		chartDir = absChartDir
+		isLocalChart = true
 	}
 
 	// check chart dependencies
@@ -45,15 +48,17 @@ func RenderTemplateE(t testing.TestingT, options *Options, chartDir string, rele
 	if options.KubectlOptions != nil && options.KubectlOptions.Namespace != "" {
 		args = append(args, "--namespace", options.KubectlOptions.Namespace)
 	}
-	args, err = getValuesArgsE(t, options, args...)
+	args, err := getValuesArgsE(t, options, args...)
 	if err != nil {
 		return "", err
 	}
 	for _, templateFile := range templateFiles {
 		// validate this is a valid template file
-		absTemplateFile := filepath.Join(absChartDir, templateFile)
-		if !strings.HasPrefix(templateFile, "charts") && !files.FileExists(absTemplateFile) {
-			return "", errors.WithStackTrace(TemplateFileNotFoundError{Path: templateFile, ChartDir: absChartDir})
+		if isLocalChart {
+			absTemplateFile := filepath.Join(chartDir, templateFile)
+			if !strings.HasPrefix(templateFile, "charts") && !files.FileExists(absTemplateFile) {
+				return "", errors.WithStackTrace(TemplateFileNotFoundError{Path: templateFile, ChartDir: chartDir})
+			}
 		}
 
 		// Note: we only get the abs template file path to check it actually exists, but the `helm template` command
